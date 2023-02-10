6814b7ab3e6d99db0e59b591b12a20e8c6a2b379
fkrein1 <felipekrein@gmail.com>
2023-02-10 12:03:41 -0300

    fix description

--- src/consts.ts
+++ src/consts.ts
@@ -2,4 +2,4 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = 'Pair Programming';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_DESCRIPTION = 'Programação, React, Typescript, Node, Golang, Algoritmos e Estruturas de Dados.';
