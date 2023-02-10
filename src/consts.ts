9aba5d0b1a68ae6a719bef5912e7a1f5329caf82
fkrein1 <felipekrein@gmail.com>
2023-02-10 11:44:45 -0300

    change img on greedy algo post

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Meu blog pessoal';
+export const SITE_TITLE = 'Pair Programming';
 export const SITE_DESCRIPTION = 'Welcome to my website!';
